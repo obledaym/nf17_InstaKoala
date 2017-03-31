0f4a1b2a86abced5f3605b465e3760ac40216206
obledaym <obled.aymeric@gmail.com>
2017-03-31 14:58:44 +0200

    Nouvelle version qui fonctionne

--- db.php
+++ db.php
@@ -1,13 +1,7 @@
 <?php
-
-  try{
-      $db= new PDO ("pgsql:dbname='Base NF17;host='tuxa.sme.utc", "nf17p109", "0cPTGNfw") or die(print_r($db->errorInfo()));
-      $db->exec("SET NAMES utf8");
-      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-}
-     
-   catch(Exeption $e){
-      die("Erreur!".$e->getMessage());
-   }
+$dbname="dbnf17p109";
+$host="tuxa.sme.utc";
+$dbuser="nf17p109";
+$dbpass="0cPTGNfw";
+$db = new PDO("pgsql:dbname=$dbname;host=$host", $dbuser, $dbpass);
 ?>
- 
